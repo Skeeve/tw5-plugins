cac18501c2d2e8404d1eef28c7141d7fe8f03757
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2018-02-23 16:23:21 +0100

    Seems travis isnn't using bash

--- timestampadjust.sh
+++ timestampadjust.sh
@@ -3,9 +3,9 @@ find "$@" -name \*.tid | while read filename; do
   # get all time stamps for the file, following renames
   allts=$( git log --follow --pretty=%at -- "$filename" )
   # get the oldest timestamp
-  created=${allts##*$'\n'}
+  created=$(echo ""$allts" | tail -1)
   # get the newest timestamp
-  modified=${allts%%$'\n'*}
+  modified=$(echo ""$allts" | head -1)
   # Now some patternmatching
   perl -MPOSIX -i -pe 'BEGIN {
       $ts{created}='$created';
