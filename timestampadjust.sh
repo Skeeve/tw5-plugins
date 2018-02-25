67f72182cc2bb1e414df8b2b8f781a85fec61d9c
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2018-02-25 15:01:35 +0100

    Removed unrequired files. Tried to fix links.

--- timestampadjust.sh
+++ timestampadjust.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This small shell / perl script is designed to find in each provided
+# directory alll *.tid files and adjust any timestamp placeholder in there.
 find "$@" -name \*.tid | while read filename; do
   # get all time stamps for the file, following renames
   allts=$( git log --follow --pretty=%at -- "$filename" )
